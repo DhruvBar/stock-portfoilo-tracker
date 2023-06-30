85989d072469010587da388afec15bf93bbbc9e4
Dhruv Baraiya <dhruvbaraiya27@gmail.com>
2023-06-30 14:03:28 +0530

    add Authentication Provider.

--- src/main/java/org/daiict/authn_authz/config/SecurityConfig.java
+++ src/main/java/org/daiict/authn_authz/config/SecurityConfig.java
@@ -2,15 +2,41 @@ package org.daiict.authn_authz.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
 public class SecurityConfig {
+
+
+    @Bean
+    public UserDetailsService getUserDetailsService()
+    {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider getDaoAuthProvider()
+    {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(getUserDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(getPasswordEncoder());
+
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -27,6 +53,7 @@ public class SecurityConfig {
                 .defaultSuccessUrl("/user/")
                 .and()
                 .csrf().disable();
+        http.authenticationProvider(getDaoAuthProvider());
         return http.build();
     }
 
