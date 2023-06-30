630ac81459f15dc97ddfe77cd34157fc1293bd7e
Dhruv Baraiya <dhruvbaraiya27@gmail.com>
2023-06-30 14:18:40 +0530

    add user details in home page.

--- src/main/java/org/daiict/authn_authz/controller/UserController.java
+++ src/main/java/org/daiict/authn_authz/controller/UserController.java
@@ -1,12 +1,29 @@
 package org.daiict.authn_authz.controller;
 
+import org.daiict.authn_authz.model.UserDetail;
+import org.daiict.authn_authz.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserRepository userRepo;
+    @ModelAttribute
+    private void userDetails(Model m, Principal p) {
+        String email = p.getName();
+        UserDetail user = userRepo.findByEmail(email);
+
+        m.addAttribute("user", user);
+
+    }
 
     @GetMapping("/")
     public String home()
