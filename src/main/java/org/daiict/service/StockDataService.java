df7826c3e01d934635251e521f01a952fcd88128
DhruvBar <202001140@daiict.ac.in>
2023-07-09 12:57:24 -0500

    moved api token with config

--- src/main/java/org/daiict/service/StockDataService.java
+++ src/main/java/org/daiict/service/StockDataService.java
@@ -1,5 +1,6 @@
 package org.daiict.service;
 
+import org.daiict.config.ConfigProperties;
 import org.daiict.model.QuoteResponse;
 import org.daiict.model.SearchStockResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,18 +15,21 @@ public class StockDataService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private ConfigProperties configProperties;
+
     public QuoteResponse.QuoteData getStockQuote(String symbol) {
-        String url = String.format("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s&apikey=8W6C042MM42CF6GP", symbol);
+        String url = String.format("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=%s&apikey=%s", symbol, configProperties.getAlphaVantageApiKey());
         QuoteResponse response = restTemplate.getForObject(url, QuoteResponse.class);
         return response.getQuoteData();
     }
 
-    public List<SearchStockResponse.SearchStockResponseData> searchStock(String query){
+    public List<SearchStockResponse.SearchStockResponseData> searchStock(String query) {
         try {
-            String url = String.format("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=%s&apikey=8W6C042MM42CF6GP", query);
+            String url = String.format("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=%s&apikey=%s", query, configProperties.getAlphaVantageApiKey());
             SearchStockResponse searchStockResponse = restTemplate.getForObject(url, SearchStockResponse.class);
             return searchStockResponse.getBestMatches();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         // returning empty list if search fails
